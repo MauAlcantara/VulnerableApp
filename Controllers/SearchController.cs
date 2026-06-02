950b0efcb6e1c665cb209c4f2ae93bf32aebd802
Miau5069 <mau506680@gmail.com>
2026-06-02 12:25:34 -0600

    practica P2B

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using VulnerableApp.Data;
+using VulnerableApp.Models;
+
+namespace VulnerableApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public SearchController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index(string search)
+        {
+            if (string.IsNullOrEmpty(search))
+                return View(new List<User>());
+
+            // AQUÍ ESTÁ LA VULNERABILIDAD: Concatenación directa de strings
+            string query = "SELECT * FROM Users WHERE Username LIKE '%" + search + "%'";
+            var users = _db.Users.FromSqlRaw(query).ToList();
+
+            return View(users);
+        }
+    }
+}
